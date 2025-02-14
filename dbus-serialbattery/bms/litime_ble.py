transistorgit/venus-os_dbus-serialbattery

commit 0a2a09a75edb6248d8ee2847d71397c9549772ab

LiTime BMS cleanup and notes added

dbus-serialbattery/bms/litime_ble.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+
+# NOTES
+# Added by https://github.com/calledit
+# https://github.com/mr-manuel/venus-os_dbus-serialbattery/pull/59
+
 from battery import Battery, Cell
-import utils
-from bms.syncron_ble import Syncron_Ble
 from struct import unpack_from
+from utils_ble import Syncron_Ble
 import time
 
 
@@ -29,7 +33,6 @@ class LiTime_Ble(Battery):
             self.address, read_characteristic="0000ffe1-0000-1000-8000-00805f9b34fb", write_characteristic="0000ffe2-0000-1000-8000-00805f9b34fb"
         )
         self.request_and_proccess_battery_staus()
-        self.get_settings()
 
         return True
 
@@ -157,12 +160,6 @@ class LiTime_Ble(Battery):
         self.capacity_remaining = remaining_amph
         self.history.total_ah_drawn = discharges_amph_count
         self.history.full_discharges = discharges_count
-
-    def get_settings(self):
-
-        self.max_battery_voltage = utils.MAX_CELL_VOLTAGE * self.cell_count
-        self.min_battery_voltage = utils.MIN_CELL_VOLTAGE * self.cell_count
-        return True
 
     def request_and_proccess_battery_staus(self):
         data = self.ble_handle.send_data(self.query_battery_status)

dbus-serialbattery/dbus-serialbattery.py
@@ -321,7 +321,7 @@ def main():
 
             if port == "LiTime_Ble":
                 # noqa: F401 --> ignore flake "imported but unused" error
-                from bms.litime import LiTime_Ble  # noqa: F401
+                from bms.litime_ble import LiTime_Ble  # noqa: F401
 
             class_ = eval(port)
 

dbus-serialbattery/config.default.ini
@@ -118,7 +118,7 @@ SOC_CALC_CURRENT_MEASURED_BY_USER = -300, 300
 ; Description:
 ;     Specify the Bluetooth BMS and its MAC address that you want to use. Leave empty to disable.
 ; Available Bluetooth BMS:
-;     Jkbms_Ble, LltJbd_Ble
+;     Jkbms_Ble, LiTime_Ble, LltJbd_Ble
 ; Example for one BMS:
 ;     BLUETOOTH_BMS = Jkbms_Ble C8:47:8C:00:00:00
 ; Example for multiple BMS:
@@ -414,7 +414,7 @@ TIME_TO_SOC_INC_FROM = False
 ;     Daly_Can, Jkbms_Can
 ;
 ; Available Bluetooth BMS:
-;     Jkbms_Ble, LltJbd_Ble
+;     Jkbms_Ble, LiTime_Ble, LltJbd_Ble
 BMS_TYPE =
 
 ; Exclude these serial devices from the driver startup.

commit 55f58ac8cb3c0d5d61aec1a7d888e29060a4059d

Fix LiTime_Ble issue and adder error handling Issue Introduced with https://github.com/mr-manuel/venus-os_dbus-serialbattery/commit/0a2a09a75edb6248d8ee2847d71397c9549772ab

dbus-serialbattery/bms/litime_ble.py
@@ -33,9 +33,12 @@ class LiTime_Ble(Battery):
         self.ble_handle = Syncron_Ble(
             self.address, read_characteristic="0000ffe1-0000-1000-8000-00805f9b34fb", write_characteristic="0000ffe2-0000-1000-8000-00805f9b34fb"
         )
-        self.request_and_proccess_battery_staus()
 
-        return True
+        if self.ble_handle.connected:
+            self.request_and_proccess_battery_staus()
+            return True
+        else:
+            return False
 
     def unique_identifier(self) -> str:
         return self.address
@@ -161,6 +164,9 @@ class LiTime_Ble(Battery):
         self.capacity_remaining = remaining_amph
         self.history.total_ah_drawn = discharges_amph_count
         self.history.full_discharges = discharges_count
+
+    def get_settings():
+        return True
 
     def request_and_proccess_battery_staus(self):
         data = self.ble_handle.send_data(self.query_battery_status)

dbus-serialbattery/dbus-serialbattery.py
@@ -323,13 +323,13 @@ def main():
                 # noqa: F401 --> ignore flake "imported but unused" error
                 from bms.kilovault_ble import Kilovault_Ble  # noqa: F401
 
-            if port == "LltJbd_Ble":
-                # noqa: F401 --> ignore flake "imported but unused" error
-                from bms.lltjbd_ble import LltJbd_Ble  # noqa: F401
-
             if port == "LiTime_Ble":
                 # noqa: F401 --> ignore flake "imported but unused" error
                 from bms.litime_ble import LiTime_Ble  # noqa: F401
+
+            if port == "LltJbd_Ble":
+                # noqa: F401 --> ignore flake "imported but unused" error
+                from bms.lltjbd_ble import LltJbd_Ble  # noqa: F401
 
             class_ = eval(port)
 

dbus-serialbattery/utils.py
@@ -14,7 +14,7 @@ import serial
 
 
 # CONSTANTS
-DRIVER_VERSION: str = "2.0.20250211dev"
+DRIVER_VERSION: str = "2.0.20250214dev"
 """
 current version of the driver
 """

dbus-serialbattery/utils_ble.py
@@ -15,6 +15,7 @@ class Syncron_Ble:
     response_event = False
     response_data = False
     main_thread = False
+    connected = False
 
     write_characteristic = None
     read_characteristic = None
@@ -42,6 +43,8 @@ class Syncron_Ble:
             logger.error("bluetooh LE thread took to long to start")
         if not connected_ok:
             logger.error(f"bluetooh LE connection to address: {self.address} took to long to inititate")
+        else:
+            self.connected = True
 
     def initiate_ble_thread_main(self):
         asyncio.run(self.async_main(self.address))